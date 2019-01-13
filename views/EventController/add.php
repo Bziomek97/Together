fae360200a1f807305279e3dcbe7e4cd8b33df31
aurora <b.ziomek97@gmail.com>
2019-01-13 21:28:06 +0100

    work!

--- views/EventController/add.php
+++ views/EventController/add.php
@@ -7,75 +7,76 @@
 <?php include(dirname(__DIR__) . '/navbar.php');
 include(dirname(__DIR__) . '/sidebar.php');?>
 
-    <div class="mt-4" style="margin-left: 100px; margin-right: 100px; background-color: whitesmoke; border-radius: 25px;">
-        <div class="container col">
-        <label class="mt-3" style="font-size: 25px;"><img src="https://img.icons8.com/ios-glyphs/30/000000/plus-math.png" width="22px" height="22px"> Add event</label>
-        </div>
-        <div class="container col">
-            <label class="mt-1" style="font-size: 18px">Basic Information</label>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+    <form method="POST" action="?page=add">
+            <div class="mt-4" style="margin-left: 100px; margin-right: 100px; background-color: whitesmoke; border-radius: 25px;">
+                <div class="container col">
+                <label class="mt-3" style="font-size: 25px;"><img src="https://img.icons8.com/ios-glyphs/30/000000/plus-math.png" width="22px" height="22px"> Add event</label>
                 </div>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            </div>
-
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">Description</span>
-                </div>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
-            </div>
-
-            <div class="container row ">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroup-sizing-default">Begin Date</span>
+                <div class="container col">
+                    <label class="mt-1" style="font-size: 18px">Basic Information</label>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                     </div>
-                    <input type="date" class="form-control col-sm-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                    <input type="time" class="form-control col-sm-1" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                </div>
 
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroup-sizing-default">End Date</span>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Description</span>
+                        </div>
+                        <textarea class="form-control" aria-label="With textarea"></textarea>
                     </div>
-                    <input type="date" class="form-control col-sm-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                    <input type="time" class="form-control col-sm-1" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+
+                    <div class="container row ">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default">Begin Date</span>
+                            </div>
+                            <input type="date" class="form-control col-sm-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            <input type="time" class="form-control col-sm-1" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default">End Date</span>
+                            </div>
+                            <input type="date" class="form-control col-sm-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            <input type="time" class="form-control col-sm-1" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        </div>
+                    </div>
+                </div>
+                <div class="container col">
+                    <label class="mt-4" style="font-size: 18px">Localization</label>
+                </div>
+                <div class="container col ">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Place</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Street and Number</span>
+                        </div>
+                        <input type="text" class="form-control col" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        <input type="text" class="form-control col-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">City</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                </div>
+
+                <div class="container col d-flex ">
+                    <button type="button" class="btn btn-primary mb-3 ml-auto">Add event</button>
                 </div>
             </div>
-        </div>
-        <div class="container col">
-            <label class="mt-4" style="font-size: 18px">Localization</label>
-        </div>
-        <div class="container col ">
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Place</span>
-                </div>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            </div>
-
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Street and Number</span>
-                </div>
-                <input type="text" class="form-control col" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                <input type="text" class="form-control col-2" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            </div>
-
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">City</span>
-                </div>
-                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-            </div>
-        </div>
-
-        <div class="container col d-flex ">
-            <button type="button" class="btn btn-primary mb-3 ml-auto">Add event</button>
-        </div>
-    </div>
-
+    </form>
 </body>
 </html>
